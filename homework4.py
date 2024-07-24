93a0097d6535af7632948c62228c69b1b45a7ebc
Elena-star-sys <elenashulgaelena@gmail.com>
2024-07-24 17:23:18 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,10 @@
+my_string = input("Приветствую вас: ")
+print(len(my_string))
+
+my_string_ = input("Введите своё имя: ")
+print("Моя строка".replace(" ",""))
+print("Моя строка"[0])
+print("Моя строка"[-1])
+print("Моя строка".upper())
+print("Моя строка". lower())
+
